4173b6dc782e39a52a1148a6e4dbd0be086c2629
Artur Z <arturziianbaev@gmail.com>
2023-11-01 17:38:31 -0700

    finish contact page

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from flask import Flask, render_template, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+import smtplib
+import os
 
+SENDER = os.environ.get("SENDER")
+PASSWORD=os.environ.get("PASSWORD")
+#email address where you want to get notification ⬇️
+RECEIVER = "arturziianbaev@gmail.com"
+
+class ContactForm(FlaskForm):
+    email = StringField(label='Email:', validators=[DataRequired(), Email()],
+                        render_kw={"placeholder": "arturziianbaev@gmail.com"})
+    name = StringField(label='Name:', validators=[DataRequired(), ],
+                       render_kw={"placeholder": "Artur Ziianbaev"})
+    message = StringField(label='Your message:', validators=[DataRequired(), Length(min=5)],
+                          render_kw={"placeholder": "Hi, my name is Artur Ziianbaev. I like your portfolio and ..."})
+    submit=SubmitField(label='Submit')
 
 
 app=Flask(__name__)
 Bootstrap5(app)
-
+app.config['SECRET_KEY'] ="asd"
 app.config['SQLALCHEMY_DATABASE_URI'] =('sqlite:///projects.db')
 db = SQLAlchemy()
 db.init_app(app)
@@ -58,9 +76,27 @@ def projects():
     #     list.append(_)
     return render_template('projects.html',projects=projects)
 
-@app.route("/contact")
+@app.route("/contact",methods=['POST','GET'])
 def contact():
-    return render_template('contact.html')
+    form=ContactForm()
+    if form.validate_on_submit():
+        email=form.email.data
+        name=form.name.data
+        message=form.message.data
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+
+            connection.starttls()
+            connection.login(user=SENDER,
+                             password=PASSWORD)
+            connection.sendmail(
+                from_addr=SENDER,
+                to_addrs=RECEIVER,
+                msg=f"Subject:Portfolio\n\nHere is the message from {name} his/her email: {email}.\nMessage: {message}"
+            )
+        return render_template('contact.html')
+
+
+    return render_template('contact.html',form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
